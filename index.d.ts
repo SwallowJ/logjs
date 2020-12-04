99c484820fb1a8ef5ffb42f6acbcb22b68ee9916
Daisy <jfh52020@163.com>
2020-12-04 20:39:54 +0800

    add_pository

--- index.d.ts
+++ index.d.ts
@@ -1,53 +1,34 @@
-interface optionsType {
-    /**
-     * logger name
-     * default = "main"
-     */
-    name?: string;
-
-    /**
-     * if log the stack
-     * unrealized
-     */
-    stack?: boolean;
-
-    /**
-     * log file path
-     * is undefind? no log file
-     */
-    filePath?: string;
-}
-
-enum log_level {
+import { LOGGER } from './lib/typing';
+declare enum log_level {
     DEBUG = 0,
-    INFO,
-    WARN,
-    ERROR,
-    FATAL,
+    INFO = 1,
+    WARN = 2,
+    ERROR = 3,
+    FATAL = 4,
 }
-
-export default class Logger {
-    constructor(options?: optionsType);
-
+export default class Logger implements LOGGER.logger {
+    private static defaultFilePath;
+    private static objArr;
+    static levelType: typeof log_level;
+    private stack;
+    private name;
+    private writeStream?;
+    private level;
+    constructor(options?: LOGGER.optionsType);
+    private output;
     Debug(...message: any): void;
-
     Info(...message: any): void;
-
     Warn(...message: any): void;
-
     Error(...message: any): void;
-
     Fatal(...message: any): void;
-
+    private getException;
+    private getStack;
     close(): void;
-
-    setLevel(level: log_level): Logger;
-
+    setLevel(level: log_level): this;
     static clear(): void;
-
-    static New(options?: optionsType): Logger;
-
+    static New(options?: LOGGER.optionsType): Logger;
     static SetDefaultPath(defaultFilePath: string): typeof Logger;
-
-    public static CloseAll(): void;
+    static CloseAll(): void;
 }
+export {};
+//# sourceMappingURL=index.d.ts.map
